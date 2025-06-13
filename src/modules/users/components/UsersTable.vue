e41d71c88a520216b1b632e88207afe7be8d6a71
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-06-13 13:38:27 -0500

    ✨users: enhance UsersTable component with loading state Updated the UsersTable component to include a loading state using a skeleton loader, improving user experience during data fetching.

--- src/modules/users/components/UsersTable.vue
+++ src/modules/users/components/UsersTable.vue
@@ -2,9 +2,9 @@
 import type { User } from '@/domain/users/models/User'
 import { headers } from '@/modules/users/constants/table'
 import { getInitials } from '@/shared/utils/textsFormat'
-
 defineProps<{
   users: User[]
+  isLoading: boolean
 }>()
 
 const emit = defineEmits<{
@@ -17,27 +17,38 @@ function openUser(user: User) {
 </script>
 
 <template>
-  <v-card rounded="lg" elevation="4">
-    <v-data-table :headers="headers" :items="users" :items-per-page="10" show-current-page>
-      <template #item="{ item }">
-        <tr>
-          <td>
-            <v-avatar size="32">
-              <img v-if="item.avatarUrl" :src="item.avatarUrl" :alt="item.name" />
-              <span v-else class="avatar-fallback">{{ getInitials(item.name) }}</span>
-            </v-avatar>
-          </td>
-          <td>{{ item.name }}</td>
-          <td>
-            {{ item.email }}
-          </td>
-          <td>
-            <v-btn icon="$eyePlus" variant="flat" @click="openUser(item)"></v-btn>
-          </td>
-        </tr>
-      </template>
-    </v-data-table>
-  </v-card>
+  <section>
+    <v-card rounded="lg" elevation="4">
+      <v-data-table
+        :headers="headers"
+        :items="users"
+        :items-per-page="10"
+        show-current-page
+        :loading="isLoading"
+      >
+        <template v-slot:loading>
+          <v-skeleton-loader type="table-row@10"></v-skeleton-loader>
+        </template>
+        <template #item="{ item }">
+          <tr>
+            <td>
+              <v-avatar size="32">
+                <img v-if="item.avatarUrl" :src="item.avatarUrl" :alt="item.name" />
+                <span v-else class="avatar-fallback">{{ getInitials(item.name) }}</span>
+              </v-avatar>
+            </td>
+            <td>{{ item.name }}</td>
+            <td>
+              {{ item.email }}
+            </td>
+            <td>
+              <v-btn icon="$eyePlus" variant="flat" @click="openUser(item)"></v-btn>
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+    </v-card>
+  </section>
 </template>
 <style scoped lang="scss">
 .users-table {
