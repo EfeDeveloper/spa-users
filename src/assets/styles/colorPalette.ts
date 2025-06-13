a187381aa38b28326d4ed045b0aca9d70a03afd9
EdferVIIIA <edwinvilla_adsi@outlook.com>
2025-06-13 00:33:43 -0500

    ✨constants: add color palette and table headers for user interface

---
+++ src/assets/styles/colorPalette.ts
@@ -0,0 +1,17 @@
+export const palette = {
+  black: '#181A1B',
+  gray900: '#23272F',
+  gray700: '#3E434C',
+  gray500: '#767C87',
+  gray400: '#B0B4BB',
+  white: '#F5F5F5',
+  surface: '#FFFFFF',
+
+  accent: '#4ADE80',
+  boxShadow: '0 2px 8px 0 rgba(34,34,34,0.07)',
+
+  danger: '#E53E3E',
+  success: '#38A169',
+  info: '#3182CE',
+  warning: '#D69E2E',
+}
---
+++ src/modules/users/constants/table.ts
@@ -0,0 +1,7 @@
+export const headers = [
+  { title: '', key: 'avatar', sortable: false },
+  { title: 'Name', key: 'name' },
+  { title: 'Email', key: 'email' },
+
+  { title: 'Actions', key: 'actions', sortable: false },
+]
